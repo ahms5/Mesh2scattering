bfa5b4ca266dc00e771a1a45a13af8e72284d47b
Anne Heimes <ahe@akustik.rwth-aachen.de>
2025-03-07 10:21:56 +0100

    Bump version: 0.1.4 → 1.0.0

--- mesh2scattering/__init__.py
+++ mesh2scattering/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """The Mesh2hrtf and mesh2scattering developers"""
 __email__ = ''
-__version__ = '0.1.4'
+__version__ = '1.0.0'
 
 from . import input  # noqa: A004
 from . import numcalc
