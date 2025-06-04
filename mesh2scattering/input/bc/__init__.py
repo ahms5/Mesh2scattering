d56d8f87cfcedd2f9f486dac37b0851b9a3fdac7
Anne Heimes <ahe@akustik.rwth-aachen.de>
2025-06-04 17:07:31 +0200

    fix docs

--- mesh2scattering/input/bc/__init__.py
+++ mesh2scattering/input/bc/__init__.py
@@ -23,11 +23,11 @@ here is a short example of a sound soft boundary condition with a mesh with
 
     >>> import mesh2scattering.input as m2si
     >>> import pyfar as pf
-    >>> bc = m2si.BoundaryCondition(
+    >>> bc = m2si.bc.BoundaryCondition(
     >>>     kind=m2si.BoundaryConditionType.pressure,
     >>>     values=pf.FrequencyData(0, 0),
     >>> )
-    >>> mapping = m2si.BoundaryConditionMapping(100)
+    >>> mapping = m2si.bc.BoundaryConditionMapping(100)
     >>> mapping.add_material(bc, 0, 99)  # apply to all elements
     >>> mesh = m2si.Mesh(elements=100, boundary_conditions=mapping)
 
